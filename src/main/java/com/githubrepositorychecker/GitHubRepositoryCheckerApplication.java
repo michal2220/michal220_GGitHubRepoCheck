6e549dfdd383d704703727b0df3bc6574303cb0f
Michal Gawlas <gawlas05@gmail.com>
2023-08-27 11:51:24 +0200

    Initializing commit

---
+++ src/main/java/com/githubrepositorychecker/GitHubRepositoryCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.githubrepositorychecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubRepositoryCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubRepositoryCheckerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/githubrepositorychecker/GitHubRepositoryCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubrepositorychecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubRepositoryCheckerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
